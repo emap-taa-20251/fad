1bb25ab409e3095eae47e275c4409165f185cafc
Alexandre Rademaker <arademaker@gmail.com>
2025-03-19 16:38:07 -0300

    first commit

---
+++ Fad/Basic.lean
@@ -0,0 +1,43 @@
+
+def hello₁ : String := "Hello world!"
+
+#eval hello₁
+
+def hello₂ (s : String) :=
+ s!"Hello {s}!"
+
+#eval hello₂ "Alexandre"
+
+
+def factorial₀ (n : Nat) : Nat :=
+  if n = 0 then
+   1
+  else
+   n * factorial₀ (n - 1)
+
+#eval 5 - 10
+
+def factorial₁ (n : Nat) : Nat :=
+  if n ≤ 0 then
+   1
+  else
+   n * factorial₁ (n - 1)
+
+def factorial₂ (n : Nat) : Nat :=
+  if h : n == 0 then
+   1
+  else
+   have : n - 1 < n := by
+    induction n with
+    | zero => contradiction
+    | succ n ih => omega
+   n * factorial₂ (n - 1)
+
+def factorial₃ : Nat → Nat
+| 0     => 1
+| n + 1 => factorial₃ n * (n + 1)
+
+
+#eval factorial₁ 11
+#eval factorial₂ 11
+#eval factorial₃ 11
